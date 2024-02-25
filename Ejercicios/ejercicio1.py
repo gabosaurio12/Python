878fcfbac66faf06a4ee76eddf21e4728e912e39
Gabosaurio <gabgonlo05@gmail.com>
2024-02-24 21:49:49 -0600

    Feat: ejercicio2.py added

---
+++ Ejercicios/ejercicio1.py
@@ -0,0 +1,13 @@
+x = int(input("x "))
+y = int(input("y "))
+z = int(input("z "))
+
+if x > y and z > 20:
+	p = input("Introduce p ")
+	print("p = ", p)
+
+elif  z < 20:
+	print("z < 20")
+
+else:
+	print("No se leera nada")
